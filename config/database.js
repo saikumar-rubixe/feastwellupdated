f38bb84f52f6748222fc07fd6b015369a32c13d8
saikumar-rubixe <saikumar@rubixe.com>
2022-07-16 19:43:50 +0530

    db_handle

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ env.config();
 var mysql = require("mysql");
 const util = require("util");
 
-const getcon = () => {
+const getcon = (res) => {
   try {
     var con = mysql.createConnection({
       host: process.env.DB_HOST,
@@ -11,12 +11,32 @@ const getcon = () => {
       password: process.env.DB_PWD,
       database: process.env.DB_NAME,
     });
+
+    con.connect(function (err) {
+      // The server is either down
+      if (err) {
+        // or restarting (takes a while sometimes).
+        console.log("error when connecting to db:", err);
+        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+      } // to avoid a hot loop, and to allow our node script to
+    }); // process asynchronous requests in the meantime.
+    // If you’re also serving http, display a 503 error.
+    con.on("error", function (err) {
+      console.log("db error", err);
+      if (err.code === "PROTOCOL_CONNECTION_LOST") {
+        // Connection to the MySQL server is usually
+        handleDisconnect(); // lost due to either server restart, or a
+      } else {
+        // connnection idle timeout (the wait_timeout
+        throw err; // server variable configures this)
+      }
+    });
     return con;
   } catch (error) {
-    return null;
+    res.send("database connection error");
   }
 };
-const databseConnection = () => {
+const databseConnection = (res) => {
   try {
     con = getcon();
 
@@ -24,6 +44,7 @@ const databseConnection = () => {
     return runQuery;
   } catch (error) {
     console.log(`error while connecting databse ${error}`);
+    res.send("database connection error");
   }
 };
 
