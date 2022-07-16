259915b39a5799251d30363ba5dfad53a99ddf99
saikumar-rubixe <saikumar@rubixe.com>
2022-07-16 19:54:11 +0530

    s

--- config/database.js
+++ config/database.js
@@ -17,7 +17,7 @@ const getcon = (res) => {
       if (err) {
         // or restarting (takes a while sometimes).
         console.log("error when connecting to db:", err);
-        setTimeout(handleDisconnect, 2000); // We introduce a delay before attempting to reconnect,
+        setTimeout(getcon, 2000); // We introduce a delay before attempting to reconnect,
       } // to avoid a hot loop, and to allow our node script to
     }); // process asynchronous requests in the meantime.
     // If you’re also serving http, display a 503 error.
@@ -25,7 +25,7 @@ const getcon = (res) => {
       console.log("db error", err);
       if (err.code === "PROTOCOL_CONNECTION_LOST") {
         // Connection to the MySQL server is usually
-        handleDisconnect(); // lost due to either server restart, or a
+        getcon(); // lost due to either server restart, or a
       } else {
         // connnection idle timeout (the wait_timeout
         throw err; // server variable configures this)
